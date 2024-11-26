755c3837e10dd05e49224d2e6c4185bec1a0af59
carl-crl <pioquintocarladriennerafael@gmail.com>
2024-11-27 02:07:49 +0800

    Update delete-queue-rule.php
    update log: 112724

--- delete-queue-rule.php
+++ delete-queue-rule.php
@@ -2,6 +2,17 @@
 
 session_start();
 
+if (isset($_SESSION["user_id"])) {
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql = "SELECT * FROM user_tbl
+            WHERE uname_id_fld = {$_SESSION["user_id"]};";
+
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
 // Contents down below will only function if the user is set (Administrator)
 if (isset($user) && ($user["role_fld"]) == "Administrator") {
 
